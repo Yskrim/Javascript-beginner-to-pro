d24bf98bd370e3e74edd0c8dcdf549a4c1c6ab1d
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-02-06 18:31:01 +1000

    refactor: enhance cart initialization and update test structure
    - Introduced a DefaultCart constant in cart.js to streamline cart initialization when localStorage is empty.

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,25 +1,27 @@
 
 let cart;
 
-loadFromStorage();
 
+const DefaultCart = [
+    {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1',
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 2,
+        deliveryOptionId: '2',
+    },
+];
+
+loadFromStorage();
 
 function loadFromStorage(){
     cart = JSON.parse(localStorage.getItem('cart'))
 
     if(!cart){
-        cart = [
-            {
-                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity: 2,
-                deliveryOptionId: '1',
-            },
-            {
-                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                quantity: 2,
-                deliveryOptionId: '2',
-            },
-        ]
+        cart = DefaultCart
     } 
 }
 
@@ -76,4 +78,4 @@ function updateDeliveryOption(productId, deliveryOptionId){
     saveToStorage();
 }
 
-export { cart, addToCart, loadFromStorage, fetchCartQuantity, removeFromCart, updateCartQuantity, updateQuantityInStorage, saveToStorage, updateDeliveryOption }
+export { cart, DefaultCart, addToCart, loadFromStorage, fetchCartQuantity, removeFromCart, updateCartQuantity, updateQuantityInStorage, saveToStorage, updateDeliveryOption }
