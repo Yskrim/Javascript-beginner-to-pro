7d4ab1b36192f595eefbf349598096113217aeec
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-02-16 13:38:01 +1000

    17c

--- javascript-amazon-project/data/car.js
+++ javascript-amazon-project/data/car.js
@@ -1,15 +1,27 @@
 class Car {
     brand;
     model;
-
-    displayInfo(){
-        console.log(`Brand: ${this.brand}, Model: ${this.model}`);
-    }
-
+    speed;
+    
     constructor(details){
         this.brand = details.brand;
         this.model = details.model;
+        this.speed = details.speed ?? 0;
     }
+
+    displayInfo(){
+        console.log(`Brand: ${this.brand}, Model: ${this.model}, Speed: ${this.speed} km/h`);
+    };
+
+    go(){
+        if(this.speed >= 0 && this.speed < 200)
+            this.speed+=5;
+    };
+    
+    brake(){
+        if(this.speed >= 0 && this.speed < 200)
+            this.speed-=5;
+    };
 }
 
 const carsDetails = [
@@ -19,4 +31,18 @@ const carsDetails = [
 
 const Cars = carsDetails.map(obj => new Car(obj));
 
+
 Cars.forEach(car => car.displayInfo());
+console.log('\n')
+
+Cars.forEach(car => {
+    for(let i=0; i < 10; i++){car.go()}
+})
+Cars.forEach(car => car.displayInfo())
+console.log('\n')
+
+Cars.forEach(car => {
+    for(let i=0; i < 10; i++){car.brake()}
+})
+Cars.forEach(car => car.displayInfo())
+console.log('\n')
