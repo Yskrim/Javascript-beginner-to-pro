bee662ce18b2169983703d7e30f552fa38368956
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-02-23 12:09:58 +1000

    18f

--- javascript-amazon-project/scripts/lesson18.js
+++ javascript-amazon-project/scripts/lesson18.js
@@ -1,10 +1,18 @@
 async function amazon(){
-    return await fetch("https://amazon.com")
-    .then((response)=>{
-        return response.text();
-    }).then((text)=>{
-        console.log(text);
-    });
+    try {
+        return await fetch("https://amazon.com")
+        .then((response)=>{
+            return response.text();
+        }).then((text)=>{
+            console.log(text);
+        });
+    } catch(error) {
+        console.log(
+`CORS error.Your request has been blocked by the backend.
+Message:
+${error}`
+        );
+    }
 }
 
 amazon();
