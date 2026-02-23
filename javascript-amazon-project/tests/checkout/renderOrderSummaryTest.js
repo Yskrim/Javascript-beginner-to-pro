22e1933e285d9ef7c2712a6a09bb2289dad62b3a
Yskrim <141317956+Yskrim@users.noreply.github.com>
2026-02-23 13:12:30 +1000

    18j:
    refactor: update beforeAll in renderOrderSummaryTest to use async/await for improved readability

--- javascript-amazon-project/tests/checkout/renderOrderSummaryTest.js
+++ javascript-amazon-project/tests/checkout/renderOrderSummaryTest.js
@@ -7,10 +7,8 @@ import { loadProducts, loadProductsFetch } from "../../data/products.js";
 describe('Test suite: renderOrderSummary', ()=>{
     const ids = [ 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '15b6fc6f-327a-4ec4-896f-486349e85a3d'];
 
-    beforeAll((done)=>{
-        loadProductsFetch().then(()=>{
-            done();
-        });
+    beforeAll( async ()=>{
+        await loadProductsFetch();
     });
 
     beforeEach(()=>{
